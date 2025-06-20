37be79a9a20c4ac4407b05360c8ffe7d7da1a00d
Nifat130 <mahingreen130@gmail.com>
2025-06-20 23:56:21 +0600

    heading added by nifat

--- lib/core/utils/constants/image_path.dart
+++ lib/core/utils/constants/image_path.dart
@@ -3,4 +3,5 @@ class ImagePath{
    static const  String _basePath='assets/images';
 
    static const  String profileNCM = '$_basePath/profileNCM.png';
+   static const  String whatsAppBG = '$_basePath/whatsAppBG.jpg';
 }
---
+++ lib/features/whats_app/controllers/whats_app_controller.dart
@@ -0,0 +1,9 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+class WhatsAppController extends GetxController{
+
+  final messageTEController = TextEditingController();
+}
