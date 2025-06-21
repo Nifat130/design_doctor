567c9af84b009a8c070d9674080d364de95dde6a
Nifat130 <mahingreen130@gmail.com>
2025-06-21 17:09:45 +0600

    screen design done by Nifat

--- lib/features/whats_app/controllers/whats_app_controller.dart
+++ lib/features/whats_app/controllers/whats_app_controller.dart
@@ -2,8 +2,20 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
 class WhatsAppController extends GetxController{
 
   final messageTEController = TextEditingController();
+  var imagePath = "".obs;
+
+  void pickImage() async{
+    final picker = ImagePicker();
+
+    final image = await picker.pickImage(source: ImageSource.camera);
+
+    if(image != null){
+      imagePath.value = image.path;
+    }
+  }
 }
