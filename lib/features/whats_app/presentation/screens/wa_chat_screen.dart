1762ee6d879bb5cea1c9baec46dee469c88f27bb
Nifat130 <mahingreen130@gmail.com>
2025-08-04 17:32:28 +0600

    whatsApp design finally done by Nifat

--- lib/features/whats_app/presentation/screens/wa_chat_screen.dart
+++ lib/features/whats_app/presentation/screens/wa_chat_screen.dart
@@ -12,72 +12,66 @@ class WhatsAppChatScreen extends GetView<WhatsAppController> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      top: false,
-      child: Scaffold(
-        body: Container(
-          height: double.infinity,
-          width: double.infinity,
-          decoration: BoxDecoration(
-            image: DecorationImage(image: AssetImage(ImagePath.whatsAppBG), fit: BoxFit.fill),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  color: AppColors.wASecondary,
-                ),
-                child: SafeArea(
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 8.h),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Scaffold(
+      extendBody: true,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(image: AssetImage(ImagePath.whatsAppBG), fit: BoxFit.fill),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: AppColors.wASecondary,
+              ),
+              padding: EdgeInsets.only(left: 8.w, right: 8.w, top: 8.h, bottom: 16.h),
+              child: SafeArea(
+                bottom: false,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Icon(Icons.arrow_back, color: AppColors.white,),
-                            SizedBox(width: 4.w,),
-                            ClipOval(
-                              child: Image.asset(ImagePath.profileNCM, fit: BoxFit.fill, height: 40.h, width: 40.w,),
-                            ),
-                            SizedBox(width: 8.w,),
-                            CustomText(text: "Nifat ", color: AppColors.white,)
-                          ],
+                        Icon(Icons.arrow_back, color: AppColors.white,),
+                        SizedBox(width: 4.w,),
+                        ClipOval(
+                          child: Image.asset(ImagePath.profileNCM, fit: BoxFit.fill, height: 40.h, width: 40.w,),
                         ),
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          spacing: 24.w,
-                          children: [
-                            GestureDetector(
-                              onTap: (){},
-                              child: Icon(Icons.video_camera_back_outlined, color: AppColors.white,),
-                            ),
-                            GestureDetector(
-                              onTap: (){},
-                              child: Icon(Icons.call_outlined, color: AppColors.white,),
-                            ),
-                            GestureDetector(
-                              onTap: (){},
-                              child: Icon(Icons.more_vert_outlined, color: AppColors.white,),
-                            ),
-                          ],
-                        )
+                        SizedBox(width: 8.w,),
+                        CustomText(text: "Nifat ", color: AppColors.white,)
                       ],
                     ),
-                  ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      spacing: 24.w,
+                      children: [
+                        GestureDetector(
+                          onTap: (){},
+                          child: Icon(Icons.video_camera_back_outlined, color: AppColors.white,),
+                        ),
+                        GestureDetector(
+                          onTap: (){},
+                          child: Icon(Icons.call_outlined, color: AppColors.white,),
+                        ),
+                        GestureDetector(
+                          onTap: (){},
+                          child: Icon(Icons.more_vert_outlined, color: AppColors.white,),
+                        ),
+                      ],
+                    )
+                  ],
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-        bottomNavigationBar: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(image: AssetImage(ImagePath.whatsAppBG,), fit: BoxFit.fitWidth)
-          ),
-          padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 8.h),
+      ),
+      bottomNavigationBar: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8.w),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.center,
